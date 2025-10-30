da7b19fc76f9c4675b9bce998c1e2c778783b20b
Ryan Kong <ryankong200@gmail.com>
2025-10-29 20:49:34 -0400

    Update reefscape_config.js

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025ilpe",
+      "defaultValue": "2025onham2",
       "required": "true"
     },
     { "name": "Match Level",
